3512cc64ac51eaf49c739bea64c61aa4f00d9b33
nwilliamfeng <nwilliamfeng@163.com>
2019-06-18 16:23:10 +0800

    323

---
+++ Microcomm.Core/Security/ITokenIdentify.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+ 
+
+namespace Microcomm.Security
+{
+    /// <summary>
+    /// 令牌验证接口
+    /// </summary>
+    public interface ITokenIdentify
+    {
+     
+        /// <summary>
+        /// 验证token是否有效
+        /// </summary>
+        /// <param name="accessToken">要验证的token</param>
+        /// <returns></returns>
+        Task<JsonResultData<bool>> Validate(string accessToken);
+    }
+}
