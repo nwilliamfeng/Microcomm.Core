a1cbe999ba813913ff0311be96a11d71120a2e27
nwilliamfeng <nwilliamfeng@163.com>
2019-06-20 11:14:50 +0800

    添加logger

---
+++ Microcomm.Core/Utils/LogUtil.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microcomm
+{
+    public static class LogUtil
+    {
+
+        
+
+        public static ILog CreateLogger(string logName)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            return log4net.LogManager.GetLogger(logName);
+        }
+    }
+}
