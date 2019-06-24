17e598df6dcfd85e8df7d23b1265c0d8f8086b89
nwilliamfeng <nwilliamfeng@163.com>
2019-06-24 17:32:42 +0800

    修复一些bug

--- Microcomm.Core/Utils/HttpClientUtil.cs
+++ Microcomm.Core/Utils/HttpClientUtil.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace Microcomm
 {
-    public  class HttpClientUtil
+    public class HttpClientUtil
     {
         private readonly Uri _baseUrl;
-      
-        public HttpClientUtil(string baseUrl) =>  this._baseUrl= new Uri( baseUrl);
-     
-        public async Task<T> PostWithJson<T>(string path, object value,IDictionary<string,string> headers=null, IDictionary<string, string> cookie = null)
+
+        public HttpClientUtil(string baseUrl) => this._baseUrl = new Uri(baseUrl);
+
+        public async Task<T> PostWithJson<T>(string path, object value, IDictionary<string, string> headers = null, IDictionary<string, string> cookie = null)
         {
-      
+
             var cookieContainer = new CookieContainer();
             using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
             using (var client = new HttpClient(handler) { BaseAddress = _baseUrl })
@@ -36,7 +40,7 @@ namespace Microcomm
             }
         }
 
-        public async Task<T> Get<T>(string path , IDictionary<string, string> headers = null, IDictionary<string, string> cookie = null)
+        public async Task<T> Get<T>(string path, IDictionary<string, string> headers = null, IDictionary<string, string> cookie = null)
         {
 
             var cookieContainer = new CookieContainer();
@@ -55,7 +59,39 @@ namespace Microcomm
             }
         }
 
+        public async Task<string> Upload(string path, string[] filePaths, IDictionary<string, string> headers = null, IDictionary<string, string> cookie = null)
+        {
+            foreach (var filePath in filePaths)
+                if (!File.Exists(filePath))
+                    throw new ArgumentException("不存在的文件：" + filePath);
 
+            var contents = filePaths.Select(filePath => new Tuple<string, byte[]>(Path.GetFileName(filePath), File.ReadAllBytes(filePath))).ToList();
+
+            var cookieContainer = new CookieContainer();
+            using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
+            using (var client = new HttpClient(handler) { BaseAddress = _baseUrl })
+            {
+                if (cookie != null)
+                    cookie.ToList().ForEach(k => cookieContainer.Add(_baseUrl, new Cookie(k.Key, k.Value)));
+                if (headers != null)
+                    headers.ToList().ForEach(x => client.DefaultRequestHeaders.Add(x.Key, x.Value));
+                using (var content = new MultipartFormDataContent())
+                {
+                    contents.ForEach(x =>
+                    {
+                        var httpContent = new StreamContent(new MemoryStream( x.Item2));
+                        httpContent.Headers.ContentType= new System.Net.Http.Headers.MediaTypeHeaderValue(MimeTypeHelper.GetMimeType(Path.GetExtension( x.Item1)));
+                        content.Add(httpContent, "upload_"+ Path.GetFileName(x.Item1), x.Item1);
+                    });
+      
+                    using (var message = await client.PostAsync(path, content))
+                    {
+                        var input = await message.Content.ReadAsStringAsync();
+                        return input;
+                    }
+                }
+            }
+        }
 
 
     }
